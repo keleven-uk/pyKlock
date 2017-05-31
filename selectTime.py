79a8fa5e58e973f2ad050c1e3284ca5a8e628975
Kevin Scott <pyKlock@keleven.co.uk>
2017-05-31 22:21:44 +0100

    Added time displayed as Mars, Flow, Percent, Metric and Unix Time.

--- selectTime.py
+++ selectTime.py
@@ -1,38 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  4 15:42:00 2015
+
+@author: scottk2
+"""
+
 import datetime
 import time
 import math
 
-import timeCodes
+import timeCodes as tc
 
 
 class SelectTime:
+    """   A class which allows the current time to be displays in various formats.
+    The formats are held in the enum TimeTypes, these are exported.
 
-    #  GLOBAL variables used in several fimction
+    TimeType is set to the desired time format [from TimeTypes]
+    getTime is then called and this will return the current time is the desired time format."""
+
     __types = ("Fuzzy Time", "Time in Words", "GMT Time", "Local Time", "UTC Time", "Swatch Time", "New Earth Time",
                "Julian Time", "Decimal Time", "True Hex Time", "Hex Time", "Oct Time", "Binary Time", "Roman Time",
-               "Morse Time")
-
-    __hours = ("twelve", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",  "eleven", "twelve")
-    __units = ("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve")
-    __tens = ("zero", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty")
-
+               "Morse Time", "Mars Sol Date", "Coordinated Mars Time", "Flow Time", "Percent Time", "Metric Time",
+               "Unix Time")
 #
-#  the class is access bt the following properties only.
-#  timeFuncs can't be made a proper property, this seems to upset the dictionary of functions - there not callable.
+#  the class is access by the following properties only.
+#  getTime can't be made a proper property, this seems to upset the dictionary of functions - they are not callable.
 
     @property
     def timeTypes(self):
         """ Returns a tuple of available Time types."""
         return self.__types
 
-    def timeFuncs(self, position=0):
+    def getTime(self, position=0):
         """ Returns a function to return the time as position f in timeTypes."""
         return self.__funcs[position](self)
 
 
 # -------------------------------------------------------------------------------- time functions ----------------------
 #
-# The time functions can't be made property's, this seems to upset the dictionary of functions - there not callable.
+# The time functions can't be made property's, this seems to upset the dictionary of functions - they are not callable.
 #
     def __getNowTime(self):
         """  returns now as hour, munutes and seconds"""
@@ -58,10 +65,6 @@ class SelectTime:
     def getFuzzyTime(self):
         """ Returns current time as Fuzzy Time."""
 
-        minsText = {0: "", 5: "five past", 10: "ten past", 15: "quarter past", 20: "twenty past",
-                   25: "twenty-five past", 30: "half past", 35: "twenty-five to", 35: "twenty-five to",
-                   40: "twenty to", 45: "quarter to", 50: "ten to", 55: "five to", 60: ""}
-
         __hour, __mins, __secs = self.__getNowTime()
         __nrms = __mins - (__mins % 5)  # gets nearest five minutes
         __sRtn = ""
@@ -71,7 +74,7 @@ class SelectTime:
         if (__mins % 5) > 2:
             __nrms += 5  # closer to next five minutes, go to next
 
-        __sRtn = minsText[__nrms]  # look up text for minutes.
+        __sRtn = tc.minsText[__nrms]  # look up text for minutes.
 
         if __nrms > 30:
             __hour += 1
@@ -94,9 +97,9 @@ class SelectTime:
                 __hour -= 12
                 __ampm = "in the evening" if __hour > 5 else "in the afternoon"
             if __sRtn == "":
-                __fuzzyTime = "about {0}'ish {1}".format(self.__hours[__hour], __ampm)
+                __fuzzyTime = "about {0}'ish {1}".format(tc.hours[__hour], __ampm)
             else:
-                __fuzzyTime = "{0} {1} {2}".format(__sRtn, self.__hours[__hour], __ampm)
+                __fuzzyTime = "{0} {1} {2}".format(__sRtn, tc.hours[__hour], __ampm)
 
         return __fuzzyTime
 
@@ -123,17 +126,17 @@ class SelectTime:
             __ampm = "in the evening" if __hour >= 5 else "in the afternoon"
 
         if __mins == 0:
-            __minsStr = "{0} o'clock {1}".format(self.__hours[__hour], __ampm)
+            __minsStr = "{0} o'clock {1}".format(tc.hours[__hour], __ampm)
         elif 1 <= __mins <= 9:
-            __minsStr = "{0} minutes {1} {2} {3}".format(self.__units[__mins], __pasTo, self.__hours[__hour], __ampm)
+            __minsStr = "{0} minutes {1} {2} {3}".format(tc.units[__mins], __pasTo, tc.hours[__hour], __ampm)
         elif 10 <= __mins <= 20:
-            __minsStr = "{0} minutes {1} {2} {3}".format(self.__tens[__mins-9], __pasTo, self.__hours[__hour], __ampm)
+            __minsStr = "{0} minutes {1} {2} {3}".format(tc.tens[__mins-9], __pasTo, tc.hours[__hour], __ampm)
         elif 21 <= __mins <= 29:
             __minsTens = math.floor(__mins / 10)
             __minsUnit = __mins - (__minsTens * 10)
-            __minsStr = "twenty{0} minutes {1} {2} {3}".format(self.__units[__minsUnit], __pasTo, self.__hours[__hour], __ampm)
+            __minsStr = "twenty{0} minutes {1} {2} {3}".format(tc.units[__minsUnit], __pasTo, tc.hours[__hour], __ampm)
         else:
-            __minsStr = "thirty minutes past {0} {1}".format(self.__hours[__hour], __ampm)
+            __minsStr = "thirty minutes past {0} {1}".format(tc.hours[__hour], __ampm)
 
         return __minsStr
 
@@ -255,11 +258,11 @@ class SelectTime:
         """   Returns the current [local] time in Roman numerals."""
 
         __hour, __mins, __secs = self.__getNowTime()
-        
-        __Rhour = timeCodes.romanNumerals[__hour]
-        __Rmins = timeCodes.romanNumerals[__mins]
-        __Rsecs = timeCodes.romanNumerals[__secs]
-        
+
+        __Rhour = tc.romanNumerals[__hour]
+        __Rmins = tc.romanNumerals[__mins]
+        __Rsecs = tc.romanNumerals[__secs]
+
         return "{0}:{1}:{2}".format(__Rhour, __Rmins, __Rsecs)
 
 # ------------------------------------------------------------------------------------- getMorseTime -------------------
@@ -269,21 +272,102 @@ class SelectTime:
         __hour, __mins, __secs = self.__getNowTime()
 
         if __hour < 10:
-            __Mhour = "{0} {1}".format(timeCodes.morseCode[0], timeCodes.morseCode[__hour])
+            __Mhour = "{0} {1}".format(tc.morseCode[0], tc.morseCode[__hour])
         else:
-            __Mhour = "{0} {1}".format(timeCodes.morseCode[int(__hour/10)], timeCodes.morseCode[__hour % 10])
+            __Mhour = "{0} {1}".format(tc.morseCode[int(__hour/10)], tc.morseCode[__hour % 10])
 
         if __mins < 10:
-            __Mmins = "{0} {1}".format(timeCodes.morseCode[0], timeCodes.morseCode[__mins])
+            __Mmins = "{0} {1}".format(tc.morseCode[0], tc.morseCode[__mins])
         else:
-            __Mmins = "{0} {1}".format(timeCodes.morseCode[int(__mins/10)], timeCodes.morseCode[__mins % 10])
+            __Mmins = "{0} {1}".format(tc.morseCode[int(__mins/10)], tc.morseCode[__mins % 10])
 
         if __secs < 10:
-            __Msecs = "{0} {1}".format(timeCodes.morseCode[0], timeCodes.morseCode[__secs])
+            __Msecs = "{0} {1}".format(tc.morseCode[0], tc.morseCode[__secs])
         else:
-            __Msecs = "{0} {1}".format(timeCodes.morseCode[int(__secs/10)], timeCodes.morseCode[__secs % 10])
+            __Msecs = "{0} {1}".format(tc.morseCode[int(__secs/10)], tc.morseCode[__secs % 10])
 
         return "{0}:{1}:{2}".format(__Mhour, __Mmins, __Msecs)
+
+# ------------------------------------------------------------------------------------- getMarsSolDate------------------
+    def getMarsSolDate(self):
+        """   Returns the current [UTC] time as Mars Sol Date.
+
+        see http://jtauber.github.io/mars-clock/"""
+
+        __SolDataEpoch = datetime.datetime(day=6, month=1, year=2000)
+        __utcNow = datetime.datetime.utcnow()
+        __daysSinceEpoch = (__utcNow - __SolDataEpoch).days + (__utcNow - __SolDataEpoch).seconds / 86400
+
+        return "{0:5.5f}".format((__daysSinceEpoch / 1.027491252) + 44796.0 - 0.00096)
+
+# ------------------------------------------------------------------------------------- getCoordinatedMarsTime ---------
+    def getCoordinatedMarsTime(self):
+        """   Returns the current [UTC] time as Coordinated Mars Time.
+
+        see http://jtauber.github.io/mars-clock/"""
+
+        __SolDataEpoch = datetime.datetime(day=6, month=1, year=2000)
+        __utcNow = datetime.datetime.utcnow()
+        __daysSinceEpoch = (__utcNow - __SolDataEpoch).days + (__utcNow - __SolDataEpoch).seconds / 86400
+
+        __marsSolDate = (__daysSinceEpoch / 1.027491252) + 44796.0 - 0.00096
+
+        __mtc = (24 * __marsSolDate) % 24
+
+        __hour = int(__mtc)
+        __mins = (__mtc - __hour) * 60
+        __secs = (__mins - int(__mins)) * 60
+
+        return "{0:0>2.0f}:{1:0>2.0f}:{2:0>2.0f}".format(__hour, __mins, __secs)
+
+# ------------------------------------------------------------------------------------- getFlowTime -------------------
+    def getFlowTime(self):
+        """   Returns the current [local] time as Flow Time.
+        Flow Time still divides the day into 24 hours, but each hour is divided into 100 minutes of 100 seconds.
+        A Quick conversion is takes 2/3 of the minute [or second] and add it to it's self."""
+
+        __hour, __mins, __secs = self.__getNowTime()
+        __mins *= (5/3)
+        __secs *= (5/3)
+
+        return "{0:0>2}h {1:0>2.0f}m {2:0>2.0f}s".format(__hour, __mins, __secs)
+
+# ------------------------------------------------------------------------------------- getPercentTime -----------------
+    def getPercentTime(self):
+        """   Returns the current [local] time as a percent of the day.
+        See http://raywinstead.com/metricclock.shtml """
+
+        __hour, __mins, __secs = self.__getNowTime()
+
+        __noOfSeconds = (__hour * 3600) + (__mins * 60) + __secs
+        __percentSeconds = __noOfSeconds / 86400 * 100
+
+        return "{0:2.4f} PMH".format(__percentSeconds)
+
+# ------------------------------------------------------------------------------------- getMetricTime ------------------
+    def getMetricTime(self):
+        """   Returns the current [local] time in Metric time.
+        Metric time is the measure of time interval using the metric system, which defines the second as the base unit of time,
+        and multiple and submultiple units formed with metric prefixes, such as kiloseconds and milliseconds.
+        Only Kiloseconds are used here."""
+
+        __hour, __mins, __secs = self.__getNowTime()
+
+        __noOfSeconds = ((__hour * 3600) + (__mins * 60) + __secs) / 1000
+
+        return "{0} Kiloseconds".format(__noOfSeconds)
+
+# ------------------------------------------------------------------------------------- getUnixTime --------------------
+    def getUnixTime(self):
+        """   Returns UTC in Unix time.
+        Unix time, or POSIX time, is a system for describing instants in time, defined as the number of seconds
+        elapsed since midnight Coordinated Universal Time (UTC) of Thursday, January 1, 1970  """
+
+        __tday = datetime.datetime.utcnow()
+        __epoch = datetime.datetime(1970, 1, 1)
+        __secs = (__tday - __epoch).total_seconds()
+
+        return "{0:.0f}".format(__secs)
 
     #
     # GLOBAL Dictionary that holds references to all the time functions.
@@ -301,11 +385,17 @@ class SelectTime:
                "Oct Time": getOctTime,
                "Binary Time": getBinTime,
                "Roman Time": getRomanTime,
-               "Morse Time": getMorseTime}
+               "Morse Time": getMorseTime,
+               "Mars Sol Date": getMarsSolDate,
+               "Coordinated Mars Time": getCoordinatedMarsTime,
+               "Flow Time": getFlowTime,
+               "Percent Time": getPercentTime,
+               "Metric Time": getMetricTime,
+               "Unix Time": getUnixTime}
 
 
 #
-# ----------------------------------------------------- called to test, if run as main (nit imported) ------------------
+# ----------------------------------------------------- called to test, if run as main (not imported) ------------------
 #
 if __name__ == "__main__":
     import tkinter as tk
@@ -313,7 +403,7 @@ if __name__ == "__main__":
 
     def update_timeText():
         # Get the current time
-        current = s.timeFuncs(position=timeCombo.get())
+        current = s.getTime(position=timeCombo.get())
 
         # Update the timeText label box with flexi time.
         timeText.configure(text=current)
